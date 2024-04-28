5b8eb970db6827c606f5d0cc5692c59cc37cc945
irfan juliana winandar <irfanjulianawinandar@gmail.com>
2024-04-28 18:59:53 +0700

    portfolioku

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//navbar fix
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNAv = header.offsetTop;
+
+  if (window.pageYOffset > fixedNAv) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+//hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
